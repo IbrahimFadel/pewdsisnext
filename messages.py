e6b776c5560661afbe8c320dde64c3b69f7c86d4
Ibrahim Fadel <ibrahim.m.fadel@gmail.com>
2019-11-13 11:33:08 -0500

    Update messages.py

--- messages.py
+++ messages.py
@@ -7,8 +7,7 @@ messages = [
     "Day {}, @jakepaul the population of medium sized nation ({} to be exact) prefer pewdiepie's content over yours",
     "Day {}, out of the 7.8 billion human beings who inhabit this hellhole of an earth, {} prefer Pewdiepie's content to yours, @jakepaul.",
     "Day {}, don't worry @jakepaul, I'm sure you'll catch up. You only have {} more subs to go.",
-    "Day {}, I like men. One of those men is Pewdiepie, but not @jakepaul. {} people also agree with me on this.",
-    "Day {}, if I could save a certain number of people from this planet, I would save the {} people who prevent @jakepaul from overtaking Pewdiepie.",
+    "Day {}, I like alot of people. One of those people is Pewdiepie, but not @jakepaul. {} people also agree with me on this.",
     "Day {}, Pewds is next, @jakepaul? Oh really? {} people would beg to differ."
 ]
 
